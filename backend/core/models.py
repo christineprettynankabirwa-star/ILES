e1901ba1f19dcee80ef1f59b0de17d530ec701fa
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-03-28 22:45:27 +0300

    created ROLE_CHOICES under models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,4 +4,14 @@ from django.db import models
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
         ('student', 'Student'),
+        ('work_supervisor', 'Workplace Supervisor'),
+        ('acad_supervisor', 'Academic Supervisor'),
+        ('admin', 'Admin'),
     )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    university_id = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+    
