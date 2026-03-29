70756d2d2e6cdceefc7d95b9339535caed15b396
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-03-29 23:45:56 +0300

    added a new class

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -3,5 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
 admin.site.register(CustomUser, UserAdmin)
 
