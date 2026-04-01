7da53eecd92fe88da476ba6a262b22aabf71f097
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-01 23:49:54 +0300

    fixed it by unregistering the default user of django in amin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User 
 from .models import CustomUser
+
+#VERY IMPORTANT: removing the django default User admin
+admin.site.unregister(User) 
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
