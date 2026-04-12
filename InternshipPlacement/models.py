811ed7f266d2fe5e89e8942b3fcb8fe30a24fb54
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-12 22:28:13 +0300

    updated student database

--- InternshipPlacement/models.py
+++ InternshipPlacement/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# students database table 
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15) 
+    course  = models.CharField(max_length=100)
+    year_of_study = models.IntegerField()
 
-# Create your models here.
+    def __str__(self):
+        return self.name
