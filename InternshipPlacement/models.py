4670b2634870ea1177f0bccfc96742e7e24bd7c5
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-12 22:38:31 +0300

    modified internship table

--- InternshipPlacement/models.py
+++ InternshipPlacement/models.py
@@ -30,6 +30,11 @@ class internship(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     status = models.CharField(max_length=20,
+                              choices=[
+                                  ('pending', 'pending'),
+                                  ('active', 'active'),
+                                  ('completed', 'completed')
+                              ]
     )
 
     def __str__(self):
