0fb189ed227f6be39ea4a33579fbdd77a98a2e9a
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-12 23:48:19 +0300

    importing required models in admin.py

--- backend/placements/admin.py
+++ backend/placements/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+
+    #Display columns in a list view
+    list_display = ('name', 'contact_email', 'phone_number', 'industry')
+
+#Enable\adds search functionality for name and industry fields
+    search_fields = ('name', 'industry')     
+    
+#Adds a filter sidebar on the right 
+list_filter = ('industry',)
