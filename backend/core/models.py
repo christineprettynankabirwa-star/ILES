553364d223d89da6a5264f4cf4ac7155fea52c28
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-14 00:12:41 +0300

    added placement model in core(models.py)

--- backend/core/models.py
+++ backend/core/models.py
@@ -18,3 +18,38 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = "Custom User"
         verbose_name_plural = "Custom Users"
+
+
+
+class placement(models.Model):
+
+#link to the students 
+    student = models.ForeignKey(
+        'customer',
+        on_delete=models.CASADE,
+        related_name='student_placements',
+        'limit_choices_to={'role': 'student'},
+    )
+
+    accademic_supervisor = models.ForeignKey(
+        customer,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='accademic_supervisor_placements',
+        limit_choices_to={'role': 'acad_supervisor'},
+    )
+
+    #workplace details
+    organization_name = models.CharField(max_length=255)
+    workplace_supervisor_name = models.CharField(max_length=255)
+    workplace_supervisor_email = models.EmailField()
+
+    #duration 
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.username} - {self.organization_name}"
