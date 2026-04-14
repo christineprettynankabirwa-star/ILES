ec82026831fc1d4f960114d8bd863a12bab2f1f6
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-14 19:13:24 +0300

    updated more fields

--- backend/core/models.py
+++ backend/core/models.py
@@ -52,19 +52,24 @@ class InternshipPlacement(models.Model):
         limit_choices_to={'role': 'acad_supervisor'},
     )
 
+    academic_supervisor_name = models.CharField(max_length=255, blank=True, null=True)
+    academic_supervisor_contact = models.CharField(max_length=15, blank=True, null=True)
     academic_supervisor_email = models.EmailField(blank=True, null=True)
 
     #workplace details
     organization_name = models.CharField(max_length=255)
     workplace_supervisor_name = models.CharField(max_length=255)
-    workplace_supervisor_email = models.EmailField()
+    workplace_supervisor_contact = models.CharField(max_length=15, blank=True, null=True)
+    workplace_supervisor_email = models.EmailField(blank=True, null=True)
 
     #duration 
+    created_at = models.DateTimeField(auto_now_add=True)
     start_date = models.DateField()
     end_date = models.DateField()
 
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    
 
     def __str__(self):
         return f"{self.student.username} - {self.organization_name}"
