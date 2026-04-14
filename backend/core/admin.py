f93144dfcc02e7c4ae2918692b29fe286c9d73f4
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-14 12:21:42 +0300

    added autocomplete_fields

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -23,16 +23,21 @@ admin.site.register(InternshipPlacement)
 admin.site.register(WeeklyLog)
 
 #-----EvaluationCriteria------
+#-----Criteria Admin------
 class EvaluationCriteriaAdmin(admin.ModelAdmin):
     list_display = ("title", "max_score")
+    search_fields = ("title",)
 
 
 admin.site.register(EvaluationCriteria, EvaluationCriteriaAdmin)
 
 #-----Evaluation------
+#-----Evaluation Admin(FIXED)------
 class EvaluationAdmin(admin.ModelAdmin):
     list_display = ("student", "placement", "criteria", "score", "date_evaluated")
     list_filter = ("placement", "criteria")
     search_fields = ("student__username",)
+
+    autocomplete_fields = ("student", "placement", "criteria")
 
 admin.site.register(Evaluation,EvaluationAdmin)
