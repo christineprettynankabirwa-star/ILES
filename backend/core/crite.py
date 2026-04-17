14381c51631a8ed96c62dd2d0961a1bfecf3b12f
Ssali Joel <ssali2joel@gmail.com>
2026-04-16 23:29:27 -0700

    criteria for supervisor

---
+++ backend/core/crite.py
@@ -0,0 +1,8 @@
+  Evaluation Criteria (Reusable)
+class EvaluationCriteria(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
