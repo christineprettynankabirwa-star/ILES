2619b59a1a0d73af81572ddd390f37509cc629d9
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-22 20:17:33 +0300

    included Evaluation and EvaluationCriteria

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, InternshipPlacement, WeeklyLog
+from .models import CustomUser, InternshipPlacement, WeeklyLog, Evaluation, EvaluationCriteria 
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
@@ -16,9 +16,41 @@ class CustomUserAdmin(UserAdmin):
         ("Role Information", {"fields": ("role", "university_id")}),
     )
 
+
+#-----CustomUserAdmin------
+class CustomUserAdmin(admin.ModelAdmin):
+    search_fields = ("username", "email")
+    list_filter = ("role", "is_staff", "is_superuser")
+    list_display = ("username", "email", "role", "is_staff", "is_superuser")
 admin.site.register(CustomUser, CustomUserAdmin)
 
-admin.site.register(InternshipPlacement)
-admin.site.register(WeeklyLog)
+#-----InternshipPlacementAdmin------
+class InternshipPlacementAdmin(admin.ModelAdmin):
+    search_fields = ("company_name",)
+
+admin.site.register(InternshipPlacement, InternshipPlacementAdmin)
+
+class WeeklyLogAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+
+admin.site.register(WeeklyLog, WeeklyLogAdmin)
+
+#-----EvaluationCriteria------
+#-----Criteria Admin------
+class EvaluationCriteriaAdmin(admin.ModelAdmin):
+    list_display = ("title", "max_score")
+    search_fields = ("title",)
 
 
+admin.site.register(EvaluationCriteria, EvaluationCriteriaAdmin)
+
+#-----Evaluation------
+#-----Evaluation Admin(FIXED)------
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ("student", "placement", "criteria", "score", "date_evaluated")
+    list_filter = ("placement", "criteria")
+    search_fields = ("student__username",)
+
+    autocomplete_fields = ("student", "placement", "criteria")
+
+admin.site.register(Evaluation,EvaluationAdmin)
