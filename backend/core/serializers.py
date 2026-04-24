104cb6cae63376b80fc1522072fb31507c0f1c0a
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-24 12:13:16 +0300

    finished creating my first API Translator

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -2,3 +2,6 @@ from rest_framework import serializers
 from .models import WeeklyLog
 
 class WeeklyLogSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = WeeklyLog
+        fields = '__all__'
