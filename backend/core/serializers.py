adfee796e2701bc6b7854195b49dd2f15a48bf9a
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-24 12:48:34 +0300

    added Internship Serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
+#for WeeklyLog 
 from .models import WeeklyLog
 
 class WeeklyLogSerializer(serializers.ModelSerializer):
     class Meta: 
         model = WeeklyLog
+        fields = '__all__'
+
+#for InternshipPlacement
+from .models import InternshipPlacement 
+
+class InternshipPlacementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InternshipPlacement
         fields = '__all__'
