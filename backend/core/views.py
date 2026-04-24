0d0609893f66202b55735875018bdcb1d0b7970a
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-24 12:54:40 +0300

    created Internship API View which gives us : GET and POST

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
-
 #creating API view 
 from rest_framework.generics import ListCreateAPIView
 from .models import WeeklyLog
@@ -12,3 +10,14 @@ from .serializers import WeeklyLogSerializer
 class WeeklyLogListCreateAPIView(ListCreateAPIView):
     queryset = WeeklyLog.objects.all()
     serializer_class = WeeklyLogSerializer
+
+
+#creating Internship API view
+from .models import InternshipPlacement
+from .serializers import InternshipPlacementSerializer 
+from rest_framework.generics import ListCreateAPIView 
+
+class InternshipPlacementListCreateAPIView(ListCreateAPIView):
+    queryset = InternshipPlacement.objects.all()
+    serializer_class = InternshipPlacementSerializer 
+
