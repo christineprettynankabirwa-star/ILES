febe10a827b5c82e30ec3c2f8b172423bb0789e8
Ssali Joel <ssali2joel@gmail.com>
2026-04-24 18:37:21 +0300

    added tripple quotes in apps.py

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -1,5 +1,7 @@
+"""this module configures the core application for the project"""
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    """ Cnfiguration class for the core app"""
     name = 'core'
