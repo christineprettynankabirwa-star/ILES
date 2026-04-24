fe38e2e087d921c62f2c8e934fb7b4b7e19acb48
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-24 12:18:53 +0300

    created an APIView which creates an endpoint that supports POST requests to create new weekly log entries and GET requests to retrieve (fetch) all existing weekly log entries.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+
+
+
+#creating API view 
+from rest_framework.generics import ListCreateAPIView
+from .models import WeeklyLog
+from .serializers import WeeklyLogSerializer 
+
+class WeeklyLogListCreateAPIView(ListCreateAPIView):
+    queryset = WeeklyLog.objects.all()
+    serializer_class = WeeklyLogSerializer
