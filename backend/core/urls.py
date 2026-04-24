835aca9cbf26020f905c33b9979ca2ed1b1a3fc1
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-24 13:05:10 +0300

    added the url path for Internship Placement

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import WeeklyLogListCreateAPIView 
+from .views import InternshipPlacementListCreateAPIView
 
 urlpatterns = [
     path('api/weekly-logs/', WeeklyLogListCreateAPIView.as_view()),
+    path('api/internships/', InternshipPlacementListCreateAPIView.as_view()),
 ]
