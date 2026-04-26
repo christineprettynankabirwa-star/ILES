50c4b9da323df6e1d50b7cc607258945683161b6
Ssali Joel <ssali2joel@gmail.com>
2026-04-27 01:09:45 +0300

    Added URLS for Evaluation and Evaluation Criteria.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import WeeklyLogListCreateAPIView
-
+from .views import WeeklyLogListCreateAPIView,EvaluationCriteriaListCreateAPIView,EvaluationListCreateAPIView
 urlpatterns = [
     path('api/weeklylogs/',WeeklyLogListCreateAPIView.as_view()),
+    path('api/evaluationcriteria/',EvaluationCriteriaListCreateAPIView.as_view()),
+    path('api/evaluations/',EvaluationListCreateAPIView.as_view()),
 ]
