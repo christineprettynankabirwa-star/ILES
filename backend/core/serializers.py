cbd36a478e8ccb20b23956a1ee52e8de05b24d5d
Ssali Joel <ssali2joel@gmail.com>
2026-04-27 01:02:30 +0300

    Added Evaluation serializers and created Evaluation API views

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
-from .models import WeeklyLog
+from .models import WeeklyLog, EvaluationCriteria, Evaluation
 class WeeklyLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeeklyLog
+        fields = '__all__'
+class EvaluationCriteriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EvaluationCriteria
+        fields = '__all__'
+class EvaluationSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Evaluation
         fields = '__all__'
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
-from .models import WeeklyLog
-from .serializers import WeeklyLogSerializer
+from .models import WeeklyLog, EvaluationCriteria, Evaluation
+from .serializers import WeeklyLogSerializer, EvaluationCriteriaSerializer, EvaluationSerialiser
 
 class WeeklyLogListCreateAPIView(ListCreateAPIView):
     queryset = WeeklyLog.objects.all()
     serializer_class = WeeklyLogSerializer
-# Create your views here.
+class EvaluationCriteriaListCreateAPIView(ListCreateAPIView):
+    queryset = EvaluationCriteria.objects.all()
+    serializer_class = EvaluationCriteriaSerializer
+class EvaluationListCreateAPIView(ListCreateAPIView):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerialiser
