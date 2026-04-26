b099dca799d4f250c4258841357c6a226e10a3f4
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-26 23:35:57 +0300

    added urls for evaluations and evaluation criteria

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import WeeklyLogListCreateAPIView 
 from .views import InternshipPlacementListCreateAPIView
+from.views import EvaluationListCreateAPIView, EvaluationCriteriaListCreateAPIView
 
 urlpatterns = [
     path('api/weekly-logs/', WeeklyLogListCreateAPIView.as_view()),
     path('api/internships/', InternshipPlacementListCreateAPIView.as_view()),
+    path('api/evaluations/', EvaluationListCreateAPIView.as_view()),
+    path('api/evaluation-criteria/', EvaluationCriteriaListCreateAPIView.as_view()),
 ]
