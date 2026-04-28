b8288fcff56b241be77d23397b643efdb5cf7e4b
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-28 17:51:00 +0300

    resolved a conflict

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-=======
-from django.urls import path, include   
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('core.urls')),
->>>>>>> 9f3db957eed762e12f701331e3dd645dc1bf2130
 ]
