947ed4e72bc1e66d0edf400cf749def9d0d72436
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-30 16:57:12 +0300

    cleaned the React starter page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import InternshipList from "./pages/InternshipList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, Christine! My React app is alive.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ILES Internship System</h1>
+      <InternshipList />
     </div>
   );
 }
