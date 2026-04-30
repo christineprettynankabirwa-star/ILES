16558f301d7bf3ae07361a8ec7ec47004c6c203d
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-30 17:18:51 +0300

    i have written the API page inside InternshipList.js

---
+++ frontend/src/pages/InternshipList.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function InternshipList() {
+    const [internships, setInternships] = useState([]);
+
+    useEffect(()=> {
+        axios.get("http://127.0.0.1:8000/api/internships/")
+        .then(response => {
+            setInternships(response.data);
+        })
+        .catch(error => {
+            console.error("Error fetching internships:", error);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h2>Internship Placements</h2>
+
+            {internships.length === 0 ? (
+                <p>No internships found.</p>
+            ) :(
+                <u1>
+                    {internships.map(internship => (
+                        <li key={internship.id}>
+                            {internship.company_name} - {internship.position}
+                        </li>
+                    ))}
+                </u1>
+            )}
+            </div> 
+        );  
+}
+
+export default InternshipList;
