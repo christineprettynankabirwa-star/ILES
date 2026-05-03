9f3d5f32b3f2f95616c8be9d185f760e1a273747
naavapeledgie <naavajacqueline@gmail.com>
2026-05-04 00:13:57 +0300

    enforce workflow state transitions in WeeklyLogViewSet

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets, serializers
+from .models import WeeklyLog
+from .serializers import WeeklyLogSerializer
 
+class WeeklyLogViewSet(viewsets.ModelViewSet):
+    queryset = WeeklyLog.objects.all()
+    serializer_class = WeeklyLogSerializer
+
+    def perform_update(self, serializer):
+        # Retrieve the current log from the database before saving changes
+        instance = self.get_object()
+        
+        # Logic: If the log is already 'Submitted' or 'Approved', block updates
+        if instance.status in ['Submitted', 'Approved']:
+            raise serializers.ValidationError(
+                "This log has already been submitted or approved and cannot be edited."
+            )
+        
+        serializer.save()
 # Create your views here.
