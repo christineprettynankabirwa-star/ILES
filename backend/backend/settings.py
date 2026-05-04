0a39040979634eb7c2a2f6fe5e5ebd1453b60a09
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-05-04 14:30:31 +0300

    I HAVE TURNED PAGINATION ON globally

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,3 +129,8 @@ AUTH_USER_MODEL = 'core.CustomUser'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 10
+}
