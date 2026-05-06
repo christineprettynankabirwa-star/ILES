cf0f647f1c26e2a8c5b179992a56bc98d9438c9b
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-05-06 18:34:00 +0300

    updated it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import InternshipList from "./pages/InternshipList";
-import IssueForm from './pages/IssueForm'
+import IssueForm from './pages/IssueForm';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>ILES Internship System</h1>
+
+      {/* Module 2: Internship Placement Module */}
       <InternshipList />
+
+      <hr />
+
+      {/* Module  3: Weekly Log/IssueModule */}
+      <IssueForm />
     </div>
   );
 }
