2d2d1d7fcb928971c625fba2fb752f5f8942eef4
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-05-06 14:33:41 +0300

    made changes to admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -20,7 +20,9 @@ class CustomUserAdmin(admin.ModelAdmin):
 admin.site.register(CustomUser, CustomUserAdmin)
 # --- Internship Placement Admin ---
 class InternshipPlacementAdmin(admin.ModelAdmin):
-    search_fields = ['student__username', 'organization_name', 'academic_supervisor__username']
+    list_display = ("organization_name", "position", "location", "duration", "stipend",)
+    search_fields = ['student__username', 'organization_name', 'academic_supervisor__username', 'organization_name', 'position', 'location']
+    list_filter = ("location",)
     fieldsets = (   
         ("Student and university", {
             "fields": ("student", "registration_number", "course", "academic_supervisor"),
@@ -30,7 +32,7 @@ class InternshipPlacementAdmin(admin.ModelAdmin):
             "fields": ("organization_name", "workplace_supervisor_name", "workplace_supervisor_email")
         }),
         ("Placement Duration and status", {
-            "fields": ("start_date", "end_date", "is_active")
+            "fields": ("start_date", "end_date", "is_active", "position", "location", "duration", "stipend", "description",)
         }),
     )
 
