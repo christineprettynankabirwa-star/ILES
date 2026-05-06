a2f24dd37329b63520581e970e121a9170ee39f4
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-05-06 17:27:08 +0300

    created a router and register our viewsets

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-from .views import WeeklyLogListCreateAPIView,EvaluationCriteriaListCreateAPIView,EvaluationListCreateAPIView,InternshipPlacementListCreateAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter 
+from .views import IssueViewSet,InternshipPlacementViewSet,WeeklyLogListCreateAPIView,EvaluationCriteriaListCreateAPIView,EvaluationListCreateAPIView,InternshipPlacementListCreateAPIView
+
+router = DefaultRouter()
+router.register(r'issues', IssueViewSet)
+router.register(r'internshipplacements', InternshipPlacementViewSet)
+
 urlpatterns = [
     path('api/weeklylogs/',WeeklyLogListCreateAPIView.as_view()),
     path('api/internshipplacements/',InternshipPlacementListCreateAPIView.as_view()),
