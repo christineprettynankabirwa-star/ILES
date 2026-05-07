b0e9635129c405281d5705b8cddf5124b35a22b2
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-05-07 18:16:11 +0300

    created the Custom "Lock"

--- backend/core/views.py
+++ backend/core/views.py
@@ -31,8 +31,12 @@ class IssueViewSet(viewsets.ModelViewSet):
 
 # Custom Role-based "Lock"
 class IsStudentuser(permissions.BasePermission):
+    """
+    Allows access only to users with the 'student' role.
+    """
+
     def has_permission(self, request, view):
-        return (
+        return bool(
             request.user and 
             request.user.is_authenticated and
             getattr(request.user, 'role', None) == 'student'
