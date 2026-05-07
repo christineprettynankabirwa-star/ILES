022f7509195ddbe490e94fd8e3575ba2fa38814d
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-05-07 18:48:16 +0300

    corrected .permissions

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,9 +3,9 @@ from rest_framework.generics import ListCreateAPIView
 from .models import WeeklyLog, EvaluationCriteria, Evaluation, InternshipPlacement, Issue
 from .serializers import WeeklyLogSerializer, EvaluationCriteriaSerializer, EvaluationSerialiser, InternshipPlacementSerializer, IssueSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsStudentUser
 
-from .permissions import IsStudentUser
+
 
 class WeeklyLogListCreateAPIView(ListCreateAPIView):
     queryset = WeeklyLog.objects.all()
