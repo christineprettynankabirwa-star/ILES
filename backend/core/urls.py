86251d6d2f16f9a1ae791f2a289dbe65af71b2cc
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-06-06 20:19:17 +0300

    updated it to include user_profile

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
-from .views import IssueViewSet,WeeklyLogListCreateAPIView,EvaluationCriteriaListCreateAPIView,EvaluationListCreateAPIView,InternshipPlacementListCreateAPIView
+from .views import WeeklyLogListCreateAPIView,EvaluationCriteriaListCreateAPIView,EvaluationListCreateAPIView,InternshipPlacementListCreateAPIView, user_profile
 
 router = DefaultRouter()
-router.register(r'issues', IssueViewSet)
+router.register('logs', WeeklyLogListCreateAPIView, basename='log')
+router.register('placements', InternshipPlacementListCreateAPIView, basename='placement')
+router.register('evaluation-criteria', EvaluationCriteriaListCreateAPIView, basename='evaluation-criteria')
+router.register('evaluations', EvaluationListCreateAPIView, basename='evaluation')
 
 urlpatterns = [
     path('api/weeklylogs/',WeeklyLogListCreateAPIView.as_view()),
     path('api/internshipplacements/',InternshipPlacementListCreateAPIView.as_view()),
     path('api/evaluationcriteria/',EvaluationCriteriaListCreateAPIView.as_view()),
     path('api/evaluations/',EvaluationListCreateAPIView.as_view()),
+    path('api/user/profile/', user_profile, name='user-profile'),
     path('api/', include(router.urls)),
 ]
