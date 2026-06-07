bb9e6288681c27fa3f2f1d6ea84bb4b0437d9276
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-06-07 13:57:13 +0300

    created Login serializer and View

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser, Department, WeeklyLog, EvaluationCriteria, Evaluation, InternshipPlacement 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class WeeklyLogSerializer(serializers.ModelSerializer):
     class Meta:
@@ -64,5 +65,36 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         from django.contrib.auth.models import Group
         group, _ = Group.objects.get_or_create(name=user.get_role_display())
         user.groups.add(group)
-        
+
         return user
+    
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        #Add custom claims (role-based data)
+        token['username'] = user.username
+        token['email'] = user.email
+        token['department_id'] = user.department.id if user.department else None
+        token['department'] = user.department.name if user.department else None
+        token['role'] = user.role
+
+        return token
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Add user data to response
+        data['user'] = {
+            'id': self.user.id,
+            'username': self.user.username,
+            'email': self.user.email,
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+            'role': self.user.role,
+            'student_number': self.user.student_number,
+            'department': self.user.department.name if self.user.department else None,
+        }
+
+        return data
