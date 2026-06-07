00e0d3049121232157158a1667ad6068ee3c721e
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-06-07 13:44:26 +0300

    created Serializers for CustomUser and Department

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import WeeklyLog, EvaluationCriteria, Evaluation, InternshipPlacement
+from django.contrib.auth.password_validation import validate_password
+from .models import CustomUser, Department, WeeklyLog, EvaluationCriteria, Evaluation, InternshipPlacement 
+
 class WeeklyLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeeklyLog
@@ -18,4 +20,15 @@ class EvaluationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Evaluation
         fields = '__all__'
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'student_number', 'department', 'staff_number', 'phone_number']
 
