b1b1583b4af308b26bf5e8c91516129e347b3ebd
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-06-07 14:52:21 +0300

    registered the Login URL

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,24 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
-from .views import WeeklyLogListCreateAPIView,EvaluationCriteriaListCreateAPIView,EvaluationListCreateAPIView,InternshipPlacementListCreateAPIView, user_profile
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import CustomTokenObtainPairView, CustomUserViewSet, DepartmentListCreateAPIView, WeeklyLogListCreateAPIView,EvaluationCriteriaListCreateAPIView,EvaluationListCreateAPIView,InternshipPlacementListCreateAPIView, user_profile
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
+router.register('departments', DepartmentListCreateAPIView, basename='department')  
+router.register('users', CustomUserViewSet, basename='user')    
 router.register('logs', WeeklyLogListCreateAPIView, basename='log')
 router.register('placements', InternshipPlacementListCreateAPIView, basename='placement')
 router.register('evaluation-criteria', EvaluationCriteriaListCreateAPIView, basename='evaluation-criteria')
 router.register('evaluations', EvaluationListCreateAPIView, basename='evaluation')
 
 urlpatterns = [
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('api/weeklylogs/',WeeklyLogListCreateAPIView.as_view()),
     path('api/internshipplacements/',InternshipPlacementListCreateAPIView.as_view()),
     path('api/evaluationcriteria/',EvaluationCriteriaListCreateAPIView.as_view()),
     path('api/evaluations/',EvaluationListCreateAPIView.as_view()),
     path('api/user/profile/', user_profile, name='user-profile'),
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/', include(router.urls)),
 ]
