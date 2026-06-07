f14fc5b14a52b331763100299533eeef2abd8b83
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-06-07 13:50:00 +0300

    created User registration Serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -32,3 +32,37 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'student_number', 'department', 'staff_number', 'phone_number']
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'first_name', 'last_name', 'role', 'student_number', 'department', 'staff_number', 'phone_number', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            role=validated_data['role'],
+            student_number=validated_data.get('student_number'),
+            department=validated_data.get('department'),
+            staff_number=validated_data.get('staff_number'),
+            phone_number=validated_data.get('phone_number'),
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        from django.contrib.auth.models import Group
+        group, _ = Group.objects.get_or_create(name=user.get_role_display())
+        user.groups.add(group)
+        
+        return user
