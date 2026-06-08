7ab76fb912fcf9d17f0900d1381df5bc1572b209
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-06-08 22:39:29 +0300

    creted RgisterView with Generics

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from .models import CustomUser, Department, WeeklyLog, EvaluationCriteria, Evaluation, InternshipPlacement
-from .serializers import  CustomTokenObtainPairSerializer, CustomUserSerializer, DepartmentSerializer, WeeklyLogSerializer, EvaluationCriteriaSerializer, EvaluationSerializer, InternshipPlacementSerializer
+from .serializers import  UserRegistrationSerializer, CustomTokenObtainPairSerializer, CustomUserSerializer, DepartmentSerializer, WeeklyLogSerializer, EvaluationCriteriaSerializer, EvaluationSerializer, InternshipPlacementSerializer
 from rest_framework import viewsets,generics,status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .permissions import IsStudentUser
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -52,3 +52,26 @@ def user_profile(request):
         'last_name': user.last_name,
         'department': user.department.name if hasattr(user, 'department') and user.department else None,
     })
+
+
+class RegisterView(generics.CreateAPiView):
+    """User registration endpoint"""
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()    
+            return Response({
+                "user": {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                    'role': user.role
+                },                    
+                "message": "User registered successfully."
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
