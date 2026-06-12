e85f805b00fdae7d5ffc0b8a1fad876b5d54668c
naavapeledgie <naavajacqueline@gmail.com>
2026-06-12 10:14:18 +0300

    updated database password

--- backend/core/signals.py
+++ backend/core/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from .models import WeeklyLog, LogStatusHistory
+from .models import WeeklyLog
 
 @receiver(pre_save, sender=WeeklyLog)
 def capture_old_status(sender, instance, **kwargs):
